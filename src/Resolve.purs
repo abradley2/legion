93a2807f11945d182b77c0ce5c9760d49bbd61a5
tony <antbradley91@gmail.com>
2021-12-23 09:26:32 -0500

    better resolution of defense mods

--- src/Resolve.purs
+++ src/Resolve.purs
@@ -3,8 +3,9 @@ module Resolve where
 import Prelude
 import AttackRoll (rollAttacks)
 import AttackRoll as AttackRoll
-import Control.Monad.State (State, get, state)
+import Control.Monad.State (State, get, state, evalState)
 import Data.Array (catMaybes)
+import Data.Array as Array
 import Data.List (List(..))
 import Data.List as List
 import Data.Maybe (Maybe(..))
@@ -13,17 +14,10 @@ import DefenseRoll (rollDefense)
 import DefenseRoll as DefenseRoll
 import Effect (Effect)
 
-resolveAttacks ::
-  AttackRoll.Variant ->
-  ( AttackRoll.Result ->
-    Maybe AttackRoll.Value
-  ) ->
-  (Maybe DefenseRoll.Variant) ->
-  Int ->
-  Effect { attackResults :: Array (AttackRoll.Value), defenseResults :: Array (DefenseRoll.Value) }
-resolveAttacks attackVar attackSurge defenseVar attackCount = do
-  attackResults <- List.fromFoldable <<< catMaybes <$> rollAttacks attackVar attackSurge attackCount
-  pure { attackResults: [], defenseResults: [] }
+resolveAttacks attackVar attackSurge attackCount = do
+  attackResults <- List.fromFoldable <$> catMaybes <$> rollAttacks attackVar attackSurge attackCount
+  let finalAttackResults = Array.fromFoldable $ evalState (resolveDefenseMods attackResults) { dodge: 0, cover: 0 }
+  pure finalAttackResults
 
 type DefenseMods
   = { dodge :: Int
@@ -31,18 +25,18 @@ type DefenseMods
     }
 
 resolveDefenseMods :: List AttackRoll.Value -> State DefenseMods (List AttackRoll.Value)
-resolveDefenseMods Nil = do pure Nil
-
-resolveDefenseMods (Cons attack next) = do
-  result <- applyMod attack
-  case result of
-    Just hit -> (Cons hit) <$> resolveDefenseMods next
-    Nothing -> resolveDefenseMods next
+resolveDefenseMods = case _ of
+  Nil -> pure Nil
+  (Cons attack next) -> do
+    result <- applyMod attack
+    case result of
+      Just hit -> (Cons hit) <$> resolveDefenseMods next
+      Nothing -> resolveDefenseMods next
   where
-    applyMod :: AttackRoll.Value -> State DefenseMods (Maybe AttackRoll.Value)
-    applyMod AttackRoll.Crit = pure $ Just AttackRoll.Crit
-
-    applyMod AttackRoll.Hit =
+  applyMod :: AttackRoll.Value -> State DefenseMods (Maybe AttackRoll.Value)
+  applyMod = case _ of
+    AttackRoll.Crit -> pure $ Just AttackRoll.Crit
+    AttackRoll.Hit ->
       state
         ( \{ dodge, cover } ->
             if dodge > 0 then
