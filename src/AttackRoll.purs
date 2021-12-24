751b0f842bf8ae8273687ae260dc748ef17391f9
tony <antbradley91@gmail.com>
2021-12-24 08:57:16 -0500

    new attack roll flow

--- src/AttackRoll.purs
+++ src/AttackRoll.purs
@@ -1,6 +1,5 @@
 module AttackRoll
   ( Result(..)
-  , Value(..)
   , Variant(..)
   , ignoreSurge
   , rollAttacks
@@ -10,10 +9,12 @@ module AttackRoll
   ) where
 
 import Prelude
-import Control.Monad.State.Trans (StateT, state)
+
+import Control.Monad.State.Trans (StateT, evalStateT, state)
 import Control.Monad.Trans.Class (lift)
 import D8 (D8(..), rollD8)
 import Data.Array (elem, (..), replicate)
+import Data.Bifunctor (lmap)
 import Data.Enum (downFromIncluding)
 import Data.Generic.Rep (class Generic)
 import Data.List (List(..))
@@ -24,32 +25,30 @@ import Data.Traversable (sequence)
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
 
-data Value
-  = Hit
-  | Crit
-
-derive instance genericValue :: Generic Value _
-
-instance showValue :: Show Value where
-  show = genericShow
-
 data Result
-  = Result (Maybe Value)
+  = Hit
+  | Miss
+  | Crit
   | Surge
 
-applySurge :: Maybe Value -> Result -> Maybe Value
+derive instance genericResult :: Generic Result _
+
+instance showResult :: Show Result where
+  show = genericShow
+
+applySurge :: Result -> Result -> Result
 applySurge val = case _ of
-  Result res -> res
   Surge -> val
+  result -> result
 
-surgeHit :: Result -> Maybe Value
-surgeHit = applySurge $ Just Hit
+surgeHit :: Result -> Result
+surgeHit = applySurge Hit
 
-surgeCrit :: Result -> Maybe Value
-surgeCrit = applySurge $ Just Crit
+surgeCrit :: Result -> Result
+surgeCrit = applySurge Crit
 
-ignoreSurge :: Result -> Maybe Value
-ignoreSurge = applySurge Nothing
+ignoreSurge :: Result -> Result
+ignoreSurge = applySurge Surge
 
 data Variant
   = White
@@ -58,62 +57,79 @@ data Variant
 
 toResult :: Variant -> D8 -> Result
 toResult White d8
-  | elem d8 $ downFromIncluding Five = Result Nothing
+  | elem d8 $ downFromIncluding Five = Miss
   | elem d8 $ downFromIncluding Six = Surge
-  | elem d8 $ downFromIncluding Seven = Result (Just Hit)
-  | otherwise = Result (Just Crit)
+  | elem d8 $ downFromIncluding Seven = Hit
+  | otherwise = Crit
 
 toResult Black d8
-  | elem d8 $ downFromIncluding Three = Result Nothing
+  | elem d8 $ downFromIncluding Three = Miss
   | elem d8 $ downFromIncluding Four = Surge
-  | elem d8 $ downFromIncluding Seven = Result (Just Hit)
-  | otherwise = Result (Just Crit)
+  | elem d8 $ downFromIncluding Seven = Hit
+  | otherwise = Crit
 
 toResult Red d8
-  | elem d8 $ downFromIncluding One = Result Nothing
+  | elem d8 $ downFromIncluding One = Miss
   | elem d8 $ downFromIncluding Two = Surge
-  | elem d8 $ downFromIncluding Seven = Result (Just Hit)
-  | otherwise = Result (Just Crit)
+  | elem d8 $ downFromIncluding Seven = Hit
+  | otherwise = Crit
 
 type AttackMods
   = { rerolls :: Int
     , surgeTokens :: Int
     }
 
-resolveAttackMods :: Variant -> (Result -> Maybe Value) -> List Result -> StateT AttackMods Effect (List (Maybe Value))
+resolveAttackMods :: Variant -> (Result -> Result) -> List Result -> StateT AttackMods Effect (List Result)
 resolveAttackMods _ _ Nil = pure Nil
 
--- leave hits and crits alone
-resolveAttackMods variant resolveSurge (Cons (Result (Just hitOrCrit)) attacks) =
-  Cons (Just hitOrCrit)
+-- leave hits alone
+resolveAttackMods variant resolveSurge (Cons Hit attacks) =
+  Cons Hit
+    <$> resolveAttackMods variant resolveSurge attacks
+
+-- leave crits alone
+resolveAttackMods variant resolveSurge (Cons Crit attacks) =
+  Cons Crit
     <$> resolveAttackMods variant resolveSurge attacks
 
 -- check if we can re-roll misses
-resolveAttackMods variant resolveSurge (Cons (Result Nothing) attacks) = do
+resolveAttackMods variant resolveSurge (Cons Miss attacks) = do
   reroll <- lift $ resolveSurge <$> toResult variant <$> rollD8
   resolveAttackMods variant resolveSurge attacks
     >>= \next ->
         state \mods ->
           if mods.rerolls > 0 then
-            Tuple (Cons reroll next) (mods { rerolls = mods.rerolls - 1 })
+            let
+              nextMods = (mods { rerolls = mods.rerolls - 1 })
+            in
+              -- we still need to check if we can spend a surge token on the reroll
+              case reroll of
+                Surge ->
+                  if nextMods.surgeTokens > 0 then
+                    Tuple (Cons Hit next) (nextMods { surgeTokens = nextMods.surgeTokens - 1 })
+                  else
+                    Tuple (Cons Miss next) nextMods
+                _ -> Tuple (Cons reroll next) (mods { rerolls = mods.rerolls - 1 })
           else
-            Tuple (Cons Nothing next) mods
+            Tuple (Cons Miss next) mods
 
 -- check if we can spend surge tokens to convert surges
 resolveAttackMods variant resolveSurge (Cons Surge attacks) = do
-  case resolveSurge Surge of
-    Just hitOrCrit -> Cons (Just hitOrCrit) <$> resolveAttackMods variant resolveSurge attacks
-    Nothing ->
-      resolveAttackMods variant resolveSurge attacks
-        >>= \next ->
-            state \mods -> do
-              if mods.surgeTokens > 0 then
-                Tuple (Cons (Just Hit) next) (mods { surgeTokens = mods.surgeTokens - 1 })
-              else
-                Tuple (Cons Nothing next) mods
+  let
+    withSurge = resolveSurge Surge
+  next <- resolveAttackMods variant resolveSurge attacks
+  case withSurge of
+    Surge ->
+      state \mods -> do
+        if mods.surgeTokens > 0 then
+          Tuple (Cons Hit next) (mods { surgeTokens = mods.surgeTokens - 1 })
+        else
+          Tuple (Cons Miss next) mods
+    other -> pure $ Cons other next
 
-rollAttacks :: Variant -> (Result -> Maybe Value) -> Int -> Effect (List (Maybe Value))
-rollAttacks variant resolveSurge count =
-  sequence
-    $ (resolveSurge <<< toResult variant <$> rollD8)
+rollAttacks ∷ Variant → (Result → Result) → AttackMods → Int → Effect (Effect (List Result))
+rollAttacks variant resolveSurge attackMods count =
+  map (flip evalStateT attackMods <<< resolveAttackMods variant resolveSurge) $ sequence
+    $ toResult variant
+    <$> rollD8
     <$ List.fromFoldable (replicate count 0)
