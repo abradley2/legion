0b6581e8425aa889165d9aa7c084fa076d5ebd4a
tony <antbradley91@gmail.com>
2021-12-25 12:58:04 -0500

    format

--- src/Resolve.purs
+++ src/Resolve.purs
@@ -3,13 +3,10 @@ module Resolve where
 import Prelude
 import AttackRoll (rollAttacks)
 import AttackRoll as AttackRoll
-import Control.Monad.State (State, evalState, state)
 import Data.Array as Array
-import Data.List (List(..))
+import Data.List (List)
 import Data.List as List
 import Data.Maybe (Maybe(..))
-import Data.Tuple (Tuple(..))
-import DefenseRoll (rollDefense)
 import DefenseRoll as DefenseRoll
 import Effect (Effect)
 
@@ -24,5 +21,5 @@ resolveAttacks ∷ Config → Effect (List AttackRoll.Result)
 resolveAttacks { attackConfig, attackMods, attackCount, defense } = do
   attackResults <- List.fromFoldable <$> Array.fromFoldable <$> rollAttacks attackConfig attackMods attackCount
   case defense of
-    Just { defenseConfig, defenseMods } -> pure attackResults
+    Just _ -> pure attackResults
     Nothing -> pure attackResults
