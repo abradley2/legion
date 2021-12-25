65d63662628e6f413cd050b06c431150d941790f
tony <antbradley91@gmail.com>
2021-12-25 09:17:03 -0500

    dev script and some work on ui events

---
+++ dev.js
@@ -0,0 +1,38 @@
+const browserSync = require("browser-sync");
+const chokidar = require('chokidar')
+const { exec } = require('child_process')
+const { join } = require('path')
+
+chokidar.watch(join(__dirname, "./src/**/*.purs"), {
+  awaitWriteFinish: true
+}).on('change', (function () {
+  let promise = Promise.resolve()
+  let childProc = null
+  return function () {
+    if (childProc !== null) childProc.kill('SIGTERM')
+
+    promise = promise.then(function () {
+      return new Promise(function (resolve) {
+        childProc = exec("npm run build")
+
+        childProc.stdout.on('data', function (chunk) {
+          global.process.stdout.write(chunk)
+        })
+        childProc.stderr.on('data', function (chunk) {
+          global.process.stderr.write(chunk)
+        })
+
+        childProc.on('close', function () {
+          childProc = null
+          resolve()
+        })
+      })
+    })
+  }
+})())
+
+
+browserSync({
+  server: "public",
+  reloadDelay: 2000
+}).watch("**/*.js")
--- src/Main.purs
+++ src/Main.purs
@@ -3,10 +3,13 @@ module Main
   ) where
 
 import Prelude
+import AttackRoll as AttackRoll
 import CustomEvent (customEvent, dispatchDocumentEvent)
-import Data.Maybe (Maybe(..))
+import Data.Int as Int
+import Data.Maybe (Maybe(..), fromMaybe)
 import Data.Tuple (Tuple)
 import Data.Tuple.Nested (tuple3)
+import DefenseRoll as DefenseRoll
 import Effect (Effect)
 import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
@@ -19,22 +22,51 @@ import Foreign.Object as Object
 
 type Model
   = { count :: Int
+    , attackVariant :: AttackRoll.Variant
+    , attackCount :: Int
+    , defenseVariant :: Maybe DefenseRoll.Variant
+    , defenseCount :: Int
     }
 
 data Msg
   = Count
   | TriggerHelloEvent
   | ReceivedHelloEvent
+  | AttackVariantSelected AttackRoll.Variant
+  | DefenseVariantSelected (Maybe DefenseRoll.Variant)
+  | AttackCountChanged String
+  | DefenseCountChanged String
 
 triggerHelloEvent :: Aff (Maybe Msg)
 triggerHelloEvent = liftEffect $ const Nothing <$> dispatchDocumentEvent (customEvent "hello" Object.empty)
 
 init :: Tuple Model (Array (Aff (Maybe Msg)))
 init =
-  { count: 0 }
+  { count: 0
+  , attackVariant: AttackRoll.White
+  , defenseVariant: Nothing
+  , attackCount: 0
+  , defenseCount: 0
+  }
     :> []
 
 update :: Model -> Msg -> Tuple Model (Array (Aff (Maybe Msg)))
+update model (AttackCountChanged count) =
+  model { attackCount = fromMaybe 0 $ Int.fromString count }
+    :> []
+
+update model (DefenseCountChanged count) =
+  model { defenseCount = fromMaybe 0 $ Int.fromString count }
+    :> []
+
+update model (DefenseVariantSelected defenseVariant) =
+  model { defenseVariant = defenseVariant }
+    :> []
+
+update model (AttackVariantSelected attackVariant) =
+  model { attackVariant = attackVariant }
+    :> []
+
 update model Count =
   model { count = model.count + 1 }
     :> []
@@ -51,7 +83,7 @@ view model =
     [ H.button
         [ E.onClick Count
         ]
-        [ H.text $ "Clicked " <> show model.count <> " times" ]
+        [ H.text $ "Click " <> show model.count <> " times" ]
     , H.button
         [ E.onClick TriggerHelloEvent
         ]
