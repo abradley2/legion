2bda2bc65a05c5e2b7df2a9625033242b78772eb
Tony Bradley <tonybradley@tonys-mac-mini.myfiosgateway.com>
2022-01-02 10:09:31 -0500

    more fields work

--- src/Fields.purs
+++ src/Fields.purs
@@ -2,12 +2,18 @@ module Fields where
 
 import Prelude
 import AttackRoll as AttackRoll
-import Control.Monad.State (State, state)
+import Control.Monad.State (State, runState, state)
+import Data.Either (Either(..))
 import Data.List (List(..))
 import Data.Maybe (Maybe(..))
 import Data.Newtype (class Newtype, unwrap)
 import Data.Tuple (Tuple(..))
 import DefenseRoll as DefenseRoll
+
+type FieldInfo
+  = { errorLabel :: String
+    , id :: String
+    }
 
 data Toggleable a
   = Enabled a
@@ -38,64 +44,103 @@ getEnabled = case _ of
 type AttackVariant
   = AttackRoll.Variant
 
+attackVariantField :: FieldInfo
+attackVariantField = { id: "attack-variant", errorLabel: "Attack Variant" }
+
 type DefenseVariant
   = DefenseRoll.Variant
+
+defenseVariantField :: FieldInfo
+defenseVariantField = { id: "defense-variant", errorLabel: "Defense Variant" }
 
 newtype AttackCount
   = AttackCount Int
 
 derive instance newtypeAttackCount :: Newtype AttackCount _
 
+attackCountField :: FieldInfo
+attackCountField = { id: "attack-count", errorLabel: "Attack Count" }
+
 newtype AttackSurgeTokens
   = AttackSurgeTokens Int
 
 derive instance newtypeAttackSurgeTokens :: Newtype AttackSurgeTokens _
+
+attackSurgeTokensField :: FieldInfo
+attackSurgeTokensField = { id: "attack-surge-tokens", errorLabel: "Attack Surge Tokens" }
 
 newtype AimTokens
   = AimTokens Int
 
 derive instance newtypeAimTokens :: Newtype AimTokens _
 
+aimTokensField :: FieldInfo
+aimTokensField = { id: "aim-tokens", errorLabel: "Aim Tokens" }
+
 newtype DefenseSurgeTokens
   = DefenseSurgeTokens Int
 
 derive instance newtypeDefenseSurgeTokens :: Newtype DefenseSurgeTokens _
+
+defenseSurgeTokensField :: FieldInfo
+defenseSurgeTokensField = { id: "defense-surge-tokens", errorLabel: "Defense Surge Tokens" }
 
 newtype DodgeTokens
   = DodgeTokens Int
 
 derive instance newtypeDodgeTokens :: Newtype DodgeTokens _
 
+dodgeTokensField :: FieldInfo
+dodgeTokensField = { id: "dodge-tokens", errorLabel: "Dodge Tokens" }
+
 newtype Precise
   = Precise Int
 
 derive instance newtypePrecise :: Newtype Precise _
+
+preciseField :: FieldInfo
+preciseField = { id: "precise", errorLabel: "Precise" }
 
 newtype Critical
   = Critical Int
 
 derive instance newtypeCritical :: Newtype Critical _
 
+criticalField :: FieldInfo
+criticalField = { id: "critical", errorLabel: "Critical" }
+
 newtype DangerSense
   = DangerSense Int
 
 derive instance newtypeDangerSense :: Newtype DangerSense _
+
+dangerSenseField :: FieldInfo
+dangerSenseField = { id: "danger-sense", errorLabel: "Danger Sense" }
 
 newtype Cover
   = Cover Int
 
 derive instance newtypeCover :: Newtype Cover _
 
+coverField :: FieldInfo
+coverField = { id: "cover", errorLabel: "Cover" }
+
 data DefenseSurge
   = SurgeBlock
 
 derive instance eqDefenseSurge :: Eq DefenseSurge
+
+defenseSurgeField :: FieldInfo
+defenseSurgeField = { id: "defense-surge", errorLabel: "Defense Surge" }
 
 data AttackSurge
   = SurgeHit
   | SurgeCrit
 
 derive instance eqAttackSurge :: Eq AttackSurge
+
+attackSurgeField :: FieldInfo
+attackSurgeField = { id: "attack-surge", errorLabel: "Attack Surge" }
 
 type Fields
   = { attackVariant :: AttackRoll.Variant
@@ -146,75 +191,71 @@ type ValidFields
     , defenseSurge :: Maybe DefenseSurge
     }
 
-isPositive :: String -> Int -> State (List String) (Maybe Int)
-isPositive name val =
+isPositive :: FieldInfo -> Int -> State (List FieldInfo) (Maybe Int)
+isPositive fieldInfo val =
   if val < 0 then
-    state (\errors -> Tuple Nothing (Cons (name <> " cannot be less than 0") errors))
+    state (\errors -> Tuple Nothing (Cons (fieldInfo { errorLabel = fieldInfo.errorLabel <> " cannot be less than 0" }) errors))
   else
     pure $ Just val
 
-isEnabledPositive :: String -> Toggleable Int -> State (List String) (Maybe Int)
-isEnabledPositive name =
+isEnabledPositive :: FieldInfo -> Toggleable Int -> State (List FieldInfo) (Maybe Int)
+isEnabledPositive fieldInfo =
   getEnabled
     >>> case _ of
-        Just val -> isPositive name val
+        Just val -> isPositive fieldInfo val
         Nothing -> pure Nothing
 
-validateForm :: Fields -> State (List String) (Maybe ValidFields)
-validateForm fields = do
-  attackCount <- do
-    val <- isPositive "Attack Count" (unwrap fields.attackCount)
-    pure $ AttackCount <$> val
-  defenseVariant <- pure $ Just fields.defenseVariant
-  attackSurgeTokens <- do
-    val <- isEnabledPositive "Attack Surge Tokens" (unwrap <$> fields.attackSurgeTokens)
-    pure $ Just $ AttackSurgeTokens <$> val
-  defenseSurgeTokens <- do
-    val <- isEnabledPositive "Defense Surge Tokens" (unwrap <$> fields.defenseSurgeTokens)
-    pure $ Just $ DefenseSurgeTokens <$> val
-  aimTokens <- do
-    val <- isEnabledPositive "Aim Tokens" (unwrap <$> fields.aimTokens)
-    pure $ Just $ AimTokens <$> val
-  dodgeTokens <- do
-    val <- isEnabledPositive "Dodge Tokens" (unwrap <$> fields.aimTokens)
-    pure $ Just $ DodgeTokens <$> val
-  precise <- do
-    val <- isEnabledPositive "Precise" (unwrap <$> fields.precise)
-    pure $ Just $ Precise <$> val
-  critical <- do
-    val <- isEnabledPositive "Critical" (unwrap <$> fields.critical)
-    pure $ Just $ Critical <$> val
-  dangerSense <- do
-    val <- isEnabledPositive "Danger Sense" (unwrap <$> fields.dangerSense)
-    pure $ Just $ DangerSense <$> val
-  cover <- do
-    val <- isEnabledPositive "Cover" (unwrap <$> fields.cover)
-    pure $ Just $ Cover <$> val
+validateForm :: Fields -> Either (List FieldInfo) ValidFields
+validateForm =
+  validateForm_
+    >>> flip runState Nil
+    >>> case _ of
+        Tuple _ (Cons err errors) -> Left (Cons err errors)
+        Tuple (Just validFields) Nil -> Right validFields
+        Tuple Nothing Nil -> Left (Cons { id: "unknown", errorLabel: "Failed to validate form: unknown error" } Nil)
+
+validateForm_ :: Fields -> State (List FieldInfo) (Maybe ValidFields)
+validateForm_ fields = do
+  attackCount <-
+    map AttackCount
+      <$> isPositive attackCountField (unwrap fields.attackCount)
+  attackSurgeTokens <-
+    map AttackSurgeTokens
+      <$> isEnabledPositive attackSurgeTokensField (unwrap <$> fields.attackSurgeTokens)
+  defenseSurgeTokens <-
+    map DefenseSurgeTokens
+      <$> isEnabledPositive defenseSurgeTokensField (unwrap <$> fields.defenseSurgeTokens)
+  aimTokens <-
+    map AimTokens
+      <$> isEnabledPositive aimTokensField (unwrap <$> fields.aimTokens)
+  dodgeTokens <-
+    map DodgeTokens
+      <$> isEnabledPositive dodgeTokensField (unwrap <$> fields.aimTokens)
+  precise <-
+    map Precise
+      <$> isEnabledPositive preciseField (unwrap <$> fields.precise)
+  critical <-
+    map Critical
+      <$> isEnabledPositive criticalField (unwrap <$> fields.critical)
+  dangerSense <-
+    map DangerSense
+      <$> isEnabledPositive dangerSenseField (unwrap <$> fields.dangerSense)
+  cover <-
+    map Cover
+      <$> isEnabledPositive coverField (unwrap <$> fields.cover)
   pure
-    $ { attackVariant: _
-      , attackSurge: _
-      , defenseSurge: _
+    $ { attackVariant: fields.attackVariant
+      , defenseVariant: fields.defenseVariant
+      , attackSurge: fields.attackSurge
+      , defenseSurge: fields.defenseSurge
+      , attackSurgeTokens
+      , defenseSurgeTokens
+      , aimTokens
+      , dodgeTokens
+      , precise
+      , critical
+      , dangerSense
+      , cover
       , attackCount: _
-      , defenseVariant: _
-      , attackSurgeTokens: _
-      , defenseSurgeTokens: _
-      , aimTokens: _
-      , dodgeTokens: _
-      , precise: _
-      , critical: _
-      , dangerSense: _
-      , cover: _
       }
-    <$> Just fields.attackVariant
-    <*> Just fields.attackSurge
-    <*> Just fields.defenseSurge
-    <*> attackCount
-    <*> defenseVariant
-    <*> attackSurgeTokens
-    <*> defenseSurgeTokens
-    <*> aimTokens
-    <*> dodgeTokens
-    <*> precise
-    <*> critical
-    <*> dangerSense
-    <*> cover
+    <$> attackCount
